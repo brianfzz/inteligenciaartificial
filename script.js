b0500f6ac9c3e6d162b949dea02c9de9f727ac17
Yasmin <146743272+iasminzoca@users.noreply.github.com>
2025-08-21 07:47:00 -0300

    isso

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
